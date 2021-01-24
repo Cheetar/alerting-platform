3d62a46340e5fcedfae3109bc6415a377c922532
adam <ar406317@students.mimuw.edu.pl>
2021-01-24 11:21:38 +0100

    Multithreading for request-sender.py

--- request-sender/request-sender.py
+++ request-sender/request-sender.py
@@ -1,16 +1,17 @@
+import threading
 import requests
 from google.cloud import pubsub_v1
 import logging
 from decouple import config
 
-subscriber = pubsub_v1.SubscriberClient()
 subscription_path = "projects/magnetic-port-293211/subscriptions/request_topic-sub"
 
 publisher = pubsub_v1.PublisherClient()
 publisher_path = "projects/magnetic-port-293211/topics/unavailability_report"
 
 
-services_timeout = 5 # config('SERVICES_TIMEOUT')
+services_timeout = 5
+threads_number = 10
 
 
 def submit_unavailability(url):
@@ -37,8 +38,10 @@ def test_service_available(url):
 
 
 def callback(message):
+    print(threading.get_ident())
     if not 'service_url' in message.attributes:
         message.ack()
+        print("Request has not url")
         return
     if not test_service_available(message.attributes['service_url']):
         submit_unavailability(message.attributes['service_url'])
@@ -46,10 +49,22 @@ def callback(message):
 
 
 if __name__ == "__main__":
-    streaming_pull_future = subscriber.subscribe(subscription_path,
-                                                 callback=callback)
-    with subscriber:
-        try:
-            streaming_pull_future.result()
-        except TimeoutError:
-            streaming_pull_future.canel()
+    subscriber_shutdown = threading.Event()
+    futures = list()
+    subscribers = list()
+    for i in range(0, threads_number):
+        my_subscriber = pubsub_v1.SubscriberClient()
+        future = my_subscriber.subscribe(subscription_path,
+                                         callback=callback)
+        future.add_done_callback(lambda res: subscriber_shutdown.set())
+        futures.append(future)
+        subscribers.append(my_subscriber)
+    try:
+        subscriber_shutdown.wait()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+    finally:
+        for f in futures:
+            f.cancel()
+        for s in subscribers:
+            s.close()
